e2fddff94114db57a2dd4dd2d031b1c20d2020e3
solimanElmasry38 <111696217+solimanElmasry38@users.noreply.github.com>
2023-12-12 14:41:14 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,64 @@
 import express from "express";
-import cors from "cors";
 require("dotenv").config();
 import { expressMiddleware } from "@apollo/server/express4";
 import { apollo_server, server } from "./conf/apollo";
+import './conf/passport'
+
+const passport = require('passport');
+const session = require('express-session');
 
 (async function () {
   const app = express();
-  app.use(cors());
+  
   app.use(express.json());
   await apollo_server();
   app.use(express.urlencoded({ extended: true }));
   app.use("/graphql", expressMiddleware(server));
 
+  app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000,
+      },
+    })
+  );
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+
   app.get("/", (_req, res) => {
     res.send("Hello World!");
   });
 
+
+
+//auth
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (_req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/passport');
+  }
+);
+
+app.post('/api/logout', (_req, res) => {
+	res.clearCookie('connect.sid');  // clear the session cookie
+
+});
+
+
   app.listen(process.env.PORT, () => {
     console.log(`🚀 Express ready at http://localhost:${process.env.PORT}`);
-    
   });
 })();
+
+
