35e9327e533a673d5f5ab76d3086ba10349f4a32
solimanElmasry38 <solimanelmasry38@gmail.com>
2024-02-19 04:46:15 +0200

    fsfd

--- src/graphql/resolvers/product/querys/getProducts.ts
+++ src/graphql/resolvers/product/querys/getProducts.ts
@@ -1,28 +1,33 @@
 import { prisma } from "../../../../conf/prisma";
 
-
-
 type Tproduct = {
-  id: string
-  name: string
-  img: string
-  price: number
-  count: number
-  is_available: boolean
-  desc: string
-};
-export const get_products = async (): Promise<number | Tproduct[]> => {
+  id: string;
+  name: string;
+  img: string;
+  price: number;
+  count: number;
+  is_available: boolean;
+  desc: string;
  
+};
+export const get_products = async ({ input }, _contxt): Promise<Tproduct[]> => {
+  const { filter, orderByName,byCategory } = input;
 
-
-    try {
-      const product = await prisma.product.findMany();
-      if (product) {
-        return product;
-      }
-      return 0;
-    } catch (err) {
-      throw err;
-    }
-  
+  try {
+    const product = await prisma.product.findMany({
+      where: {
+        name: { contains: filter },
+        category_id:byCategory
+      },
+      orderBy: { name: orderByName },
+      include:{categorys:true}
+    });
+    // console.log(product)
+    const counttt=await prisma.product.count()
+    console.log("count is ")
+    console.log(counttt)
+    return product;
+  } catch (err) {
+    throw err;
+  }
 };
